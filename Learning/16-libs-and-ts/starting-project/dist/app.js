3fff5b3db94e9507be5fbc92e1876e4345035576
Oleg Paramey <olegparamey4@gmail.com>
2025-06-21 12:59:10 +0200

    3rd Party Libraries & TypeScript

---
+++ Learning/16-libs-and-ts/starting-project/dist/app.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+const numbers = [1, 2, 3, 4, 5];
+// split into multiple arrays
+const chunkedArray = _.chunk(numbers, 2);
+console.log(chunkedArray);
+//# sourceMappingURL=app.js.map
